b132de2e9f8d7b4931269f05731a216f6636fabd
ddophi98 <lightcloud98@gmail.com>
2023-04-26 01:17:02 +0900

    셀 하나 완성

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,7 @@
 import '../styles/App.css';
 import { useNavigate } from 'react-router';
 import { writeUserData } from '../modules/firebase/writer';
+import imgLogo from '../images/sample.png'
 
 function App() {
   const navigator = useNavigate();
@@ -10,8 +11,21 @@ function App() {
   };
   return (
     <div className="App">
-      <div>메인 화면 여기서 짜시면 됩니다.</div>
-      <button onClick={moveToAbout}>페이지이동예시</button>
+      {/* <div>메인 화면 여기서 짜시면 됩니다.</div>
+      <button onClick={moveToAbout}>페이지이동예시</button> */}
+
+      <div className='Cell' onClick={moveToAbout}>
+        <img src={imgLogo} alt='framework' className='Cell-Image'/>
+        <div className='Cell-Description'>
+          <div className='Cell-Title'>Framework</div>
+          <div className='Tag-Space'>
+            <div className='Tag' id='frontend-tag'>Frontend</div>
+            <div className='Tag' id='swift-tag'>Swift</div>
+          </div>
+        </div>
+      </div>
+
+      
     </div>
   );
 }
