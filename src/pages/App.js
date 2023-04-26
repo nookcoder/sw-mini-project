7886deb0a3c5bf478c70d9293f78fdc3e45bb370
2480258 <swh8059@naver.com>
2023-04-26 12:47:47 +0900

    added grid page and moved card to component

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,6 +3,44 @@ import { useNavigate } from 'react-router';
 import { writeUserData } from '../modules/firebase/writer';
 import imgLogo from '../images/sample.png'
 
+function Card() {
+  const navigator = useNavigate();
+  const moveToAbout = () => {
+    navigator('/about');
+    writeUserData();
+  };
+  return (
+    <div className="App">
+      {/* <div>메인 화면 여기서 짜시면 됩니다.</div>
+    <button onClick={moveToAbout}>페이지이동예시</button> */}
+
+      <div className='Cell' onClick={moveToAbout}>
+        <img src={imgLogo} alt='framework' className='Cell-Image' />
+        <div className='Cell-Description'>
+          <div className='Cell-Title'>Framework</div>
+          <div className='Tag-Space'>
+            <div className='Tag' id='frontend-tag'>Frontend</div>
+            <div className='Tag' id='swift-tag'>Swift</div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+function generateCard() {
+  let arr = []
+
+  for(let i = 0; i < 9; i++) {
+    arr.push(
+      <Card className="card"></Card>
+    )
+  }
+
+  return arr
+}
+
 function App() {
   const navigator = useNavigate();
   const moveToAbout = () => {
@@ -13,19 +51,12 @@ function App() {
     <div className="App">
       {/* <div>메인 화면 여기서 짜시면 됩니다.</div>
       <button onClick={moveToAbout}>페이지이동예시</button> */}
-
-      <div className='Cell' onClick={moveToAbout}>
-        <img src={imgLogo} alt='framework' className='Cell-Image'/>
-        <div className='Cell-Description'>
-          <div className='Cell-Title'>Framework</div>
-          <div className='Tag-Space'>
-            <div className='Tag' id='frontend-tag'>Frontend</div>
-            <div className='Tag' id='swift-tag'>Swift</div>
-          </div>
-        </div>
+      <div className="grid">
+        {generateCard()}
       </div>
 
-      
+
+
     </div>
   );
 }
