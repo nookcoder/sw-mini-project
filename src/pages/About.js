3ca3e8e229cec5afaaf5f51d8dd73890be6896e4
dudgus907 <dudgus907@naver.com>
2023-04-27 01:26:00 +0900

    'dev'

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import '../styles/About.css';
+import test from '../somacritic.png'
 
 const About = () => {
   return (
-    <div>
-      About 예시 화면
+    <div className='About'>
+      
+      <header className='Header'>
+        <nav className='Bar'>
+          <nav id='PrimaryLogo'>
+            <a className='logo'>
+              <img src={test} width='50' height='50'/>
+            </a>
+            <a className='logoWord'>
+              SomaCritic
+            </a>
+          </nav>
+        
+          
+          
+        </nav>
+      </header>
     </div>
   )
 }
