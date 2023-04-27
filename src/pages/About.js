07b0cc4e3d2386ac5413ab79c1ad9d06b15a8dce
dudgus907 <dudgus907@naver.com>
2023-04-27 16:28:24 +0900

    dev

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../styles/About.css';
+import {ReactComponent as star} from '../star2.svg'
+import Chip from '@mui/material/Chip'
 
 const About = () => {
   return (
@@ -14,20 +16,24 @@ const About = () => {
           <div className='substance'>
             <div className='Title'>
               <text className='TitleName'>React</text>
-              <img src='img/somacritic.png'></img>
-              <text>4.68</text>
-
+              <img src='img/star2.svg' id='titleStar'></img>
+              <text className='Rating'>4.68</text>
             </div>
-
             <div className='Summary'>
                 This package provides a wrapper around Dart implementations of SVG parsing, including SVG path data. In particular, it provides efficient BytesLoader implementations for package:vector_graphics. This package is easier to use but not as performant as using the vector_graphics and vector_graphics_compiler packages directly. Those packages allow you to do ahead-of-time compilation and optimization of SVGs, and offer some more performant rasterization strategies at runtime. 
             </div>
+            <div className='Tags'>
+            <Chip label="Clickable Link" component="a" href="#basic-chip" clickable />
+      <Chip
+        label="Clickable Link"
+        component="a"
+        href="#basic-chip"
+        variant="outlined"
+        clickable
+      />
+            
+            </div>
           </div>
-
-          
-          
-
-
         </div>
         
 
@@ -37,5 +43,6 @@ const About = () => {
     </div>
   )
 }
+
 
 export default About
